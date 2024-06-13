12081192846fc7d33e271336b5ae0204b861ad30
Firdavs Turdiev <TurdievFKh@akbarsdigital.ru>
2024-06-13 08:36:50 +0300

    v13

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "iOS.Service.Network",
     platforms: [
-        .iOS(.v15),
+        .iOS(.v13),
     ],
     products: [
         .library(
